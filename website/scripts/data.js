a452b67febb8d91fcebf02e9f0a386efabecbbfc
Sam Kagan <HungryJoe@users.noreply.github.com>
2019-12-13 18:58:10 -0800

    Turned query comments into queries for laws

--- website/scripts/data.js
+++ website/scripts/data.js
@@ -37,46 +37,46 @@ let data = {
   },
   law: {
     getName: function (id) {
-      //SELECT law_name FROM Laws WHERE law_id = "[id]";
+      result = sql.query("SELECT law_name FROM Laws WHERE law_id = \"" + id + "\"");
       return "Citizenship for Children of Military Members and Civil Servants Act";
     },
     // returns [[legislator_0_id, legislator_0_name], [legislator_1_id, legislator_1_name], ...]
     getSponsors: function (id) {
-      //SELECT Le.leg_id, Le.leg_name FROM Legislators AS Le
-      //  JOIN Sponsoring AS S ON Le.leg_id = S.leg_id
-      //  JOIN Laws AS La ON S.bill_id = La.bill_id
-      //  WHERE La.law_id = "[id]";
+      result = sql.query("SELECT Le.leg_id, Le.leg_name FROM Legislators AS Le" +
+                         "JOIN Sponsoring AS S ON Le.leg_id = S.leg_id" +
+                         "JOIN Laws AS La ON S.bill_id = La.bill_id" +
+                         "WHERE La.law_id = \"" + id + "\"");
       return [[0, "Dick Durbin"], [1, "Chuck Schumer"]];
     },
     getIntroductionDate: function(id) {
-      //SELECT law_intro_date FROM Laws WHERE law_id = "[id]";
+      result = sql.query("SELECT law_intro_date FROM Laws WHERE law_id = \"" + id + "\"");
       return "12/3/2019";
     },
     getSignDate: function(id) {
-      //SELECT law_sign_date FROM Laws WHERE law_id = "[id]";
+      result = sql.query("SELECT law_sign_date FROM Laws WHERE law_id = \"" + id + "\"");
       return "12/10/2019";
     },
     // returns [[legislator_0_id, legislator_0_name], [legislator_1_id, legislator_1_name], ...]
     getVotesFor: function(id) {
-      //SELECT Le.leg_id, Le.leg_name FROM Legislators AS Le
-      //  JOIN Voting AS Vg ON Le.leg_id = Vg.leg_id'
-      //  JOIN Votes AS Vs ON Vs.vote_id = Vg.vote_id
-      //  JOIN Laws AS La ON Vg.bill_id = La.bill_id
-      //  WHERE La.law_id = "[id]" AND UPPER(Vs.vote_name) = "YES";
+      result = sql.query("SELECT Le.leg_id, Le.leg_name FROM Legislators AS Le" +
+                         "JOIN Voting AS Vg ON Le.leg_id = Vg.leg_id" +
+                         "JOIN Votes AS Vs ON Vs.vote_id = Vg.vote_id" +
+                         "JOIN Laws AS La ON Vg.bill_id = La.bill_id" +
+                         "WHERE La.law_id = \"" + id + "\" AND UPPER(Vs.vote_name) = \"YES\"");
       return [[0, "Dick Durbin"], [1, "Chuck Schumer"]];
     },
     // returns [[legislator_0_id, legislator_0_name], [legislator_1_id, legislator_1_name], ...]
     getVotesAgainst: function(id) {
-      //SELECT Le.leg_id, Le.leg_name FROM Legislators AS Le
-      //  JOIN Voting AS Vg ON Le.leg_id = Vg.leg_id'
-      //  JOIN Votes AS Vs ON Vs.vote_id = Vg.vote_id
-      //  JOIN Laws AS La ON Vg.bill_id = La.bill_id
-      //  WHERE La.law_id = "[id]" AND UPPER(Vs.vote_name) = "NO";
+      result = sql.query("SELECT Le.leg_id, Le.leg_name FROM Legislators AS Le" +
+                         "JOIN Voting AS Vg ON Le.leg_id = Vg.leg_id" +
+                         "JOIN Votes AS Vs ON Vs.vote_id = Vg.vote_id" +
+                         "JOIN Laws AS La ON Vg.bill_id = La.bill_id" +
+                         "WHERE La.law_id = \"" + id + "\" AND UPPER(Vs.vote_name) = \"NO\"");
       return [[2, "Brandon"], [3, "Sam"]];
     },
     // deletes the law with the given id. Returns whether the deletion was successful.
     delete: function(id) {
-      //DELETE FROM Laws WHERE law_id = "[id]";
+      result = sql.query("DELETE FROM Laws WHERE law_id = \"" + id + "\"");
       return true;
     },
   },
