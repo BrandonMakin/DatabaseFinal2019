f07d9d1cec3eb8dda66fc5b81d34776a623ec29c
Brandon Makin <bmicmak@gmail.com>
2019-12-13 23:50:44 -0800

    added async

--- website/scripts/bill.js
+++ website/scripts/bill.js
@@ -4,24 +4,26 @@ let id = util.getIdOrRedirect(); // bill id
 ///////////////////////////////////
 // TITLE
 //
-document.getElementsByTagName('title')[0].innerHTML = id + " (Bill)"
+document.getElementsByTagName('title')[0].innerHTML = id + " (Bill)";
 
-///////////////////////////////////
-// SECTION - SUMMARY
-//
-// Display the bill Id
-document.getElementById('bill-id').innerHTML = id
-// Display the bill name
-document.getElementById('bill-name').innerHTML = data.bill.getName(id)
-// Display the date that the bill was introduced
-document.getElementById('bill-date').innerHTML = data.bill.getIntroductionDate(id)
-// Enable delete button
-util.enableDeleteButton(id, () => data.bill.delete(id));
+(async () => {
+  ///////////////////////////////////
+  // SECTION - SUMMARY
+  //
+  // Display the bill Id
+  document.getElementById('bill-id').innerHTML = id
+  // Display the bill name
+  document.getElementById('bill-name').innerHTML = await data.bill.getName(id)
+  // Display the date that the bill was introduced
+  document.getElementById('bill-date').innerHTML = await data.bill.getIntroductionDate(id)
+  // Enable delete button
+  util.enableDeleteButton(id, () => data.bill.delete(id));
 
-///////////////////////////////////
-// SECTION - SPONSORS
-//
-// For each sponsor, add the sponsor to the #sponsor-list element
-let sponsorList = document.getElementById('sponsor-list');
-let sponsors = data.bill.getSponsors(id)
-util.populateList(sponsorList, sponsors, "legislator.html")
+  ///////////////////////////////////
+  // SECTION - SPONSORS
+  //
+  // For each sponsor, add the sponsor to the #sponsor-list element
+  let sponsorList = document.getElementById('sponsor-list');
+  let sponsors = await data.bill.getSponsors(id)
+  util.populateList(sponsorList, sponsors, "legislator.html")
+})()
